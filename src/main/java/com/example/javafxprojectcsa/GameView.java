03440f4e8a97d642edd84dc2648fd4753d757606
himanimanju <insaneinkwebdev23@gmail.com>
2024-03-29 23:22:53 -0700

    losing it

--- src/main/java/com/example/javafxprojectcsa/GameView.java
+++ src/main/java/com/example/javafxprojectcsa/GameView.java
@@ -5,9 +5,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.util.Random;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.QuadCurve;
+
+import javafx.animation.PathTransition;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.net.URL;
@@ -16,6 +24,7 @@ import java.util.ResourceBundle;
 public class GameView implements Initializable{
     private int score;
     private double angle;
+    private int successfulShots = 0;
 
     @FXML
     TextField angleText;
@@ -25,7 +34,17 @@ public class GameView implements Initializable{
     Button shootBtn;
     @FXML
     ImageView hoopImage;
+    @FXML
+    ImageView ballImage;
+    @FXML
+    Label shotsMadeLabel; // need to add label in scene builder to display successful shots :((((
+    @FXML
+    private BorderPane borderPane;
 
+    ///trying
+    private double[] pathX;
+    private double[] pathY;
+    //private int pathIndex = 0;
 
     @FXML
     public void initialize(URL location, ResourceBundle resources)
@@ -33,6 +52,23 @@ public class GameView implements Initializable{
         File file = new File("src/main/resources/Image/hoopImage.png");
         Image image = new Image(file.toURI().toString());
         hoopImage.setImage(image);
+
+/*TRYING TO FIGURE OUT HOW TO ADD THE BBALL IMAGE IDK HOW
+        Image image = new Image(new File("src/main/resources/Image/basketball.png").toURI().toString());
+        ballImage.setImage(image);
+
+        // Create an ImageView with the loaded image
+        ImageView imageView = new ImageView(image);
+
+        // Set additional properties of the ImageView (position, size, etc.)
+        imageView.setX(100);
+        imageView.setY(100);
+        imageView.setFitWidth(50);
+        imageView.setFitHeight(50);
+
+        // Add the ImageView to the rootPane
+        borderPane.getChildren().add(imageView);
+        */
     }
     //does the action for the angle button on the screen
     @FXML
@@ -49,7 +85,7 @@ public class GameView implements Initializable{
         //convert text to number
         //if it is a valid angle, show shoot button and allow the person to shoot
         try {
-            number = Integer.valueOf(input);
+            number = Integer.parseInt(input);
             System.out.println("Converted integer: " + number);
             if(number < 0 || number > 90)
             {
@@ -89,6 +125,115 @@ public class GameView implements Initializable{
         midY = startY - (startX*Math.tan(angle) + 0.5*9.8*Math.pow(y, 2));
         // Circle circ1 = new Circle();
 
+        //trying
+        // Calculate shot path
+        calculateShotPath(startX, startY, endX, endY);
+/*ANOTHER TRY TO ADD THE BBALL IMAGE - CONFUSED
+        // Create basketball image
+        Image ballImage = new Image("basketball.png");
+        ImageView ballImageView = new ImageView(ballImage);
+        ballImageView.setFitWidth(20);
+        ballImageView.setFitHeight(20);
+        ballImageView.setX(startX);
+        ballImageView.setY(startY);
+
+        // Add basketball image to the scene
+        // Replace rootPane with the name of your layout container
+        borderPane.getChildren().add(ballImageView);
+
+        // Trace the shot path with the basketball image
+        traceShotPath(ballImageView);
+*/
+        /////////////////////himani added to see if we could identify if shot is made and record it
+        if (isShotMade(endX, endY)) {
+            System.out.println("Shot made!");
+            successfulShots++;
+            updateShotsMadeLabel();
+        } else {
+            System.out.println("Shot missed!");
+        }
+    }
+
+    //trying
+    private void calculateShotPath(double startX, double startY, double endX, double endY) {
+        // Calculate the number of points along the shot path
+        int numPoints = 100;
+        pathX = new double[numPoints];
+        pathY = new double[numPoints];
+
+        // Calculate coordinates of each point along the shot path
+        for (int i = 0; i < numPoints; i++) {
+            double t = (double) i / (numPoints - 1);
+            double x = (1 - t) * startX + t * endX;
+            double y = (1 - t) * startY + t * endY;
+            pathX[i] = x;
+            pathY[i] = y;
+        }
+    }
+
+    private void traceShotPath(ImageView ballImageView) {
+        // Create a PathTransition to animate the basketball along the shot path
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setNode(ballImageView);
+
+        // Create a Line object representing the shot path
+        Line path = new Line();
+        path.setStartX(pathX[0]);
+        path.setStartY(pathY[0]);
+        path.setEndX(pathX[1]);
+        path.setEndY(pathY[1]);
+
+        // Set the duration of the path transition
+        pathTransition.setDuration(Duration.seconds(2)); // Adjust duration as needed
+
+        // Set the path of the path transition
+        pathTransition.setPath(path);
+
+        // Play the path transition animation
+        pathTransition.play();
+    }
+
+    /////////////////used chatgpt for this to get started - one way to identify if shot is made or not
+    private boolean isShotMade(double endX, double endY) {
+        // Logic to determine if the shot is made based on end coordinates
+        // Example: Check if the end coordinates are within the hoop area
+        // You need to implement this method according to your game's logic
+        // For demonstration purposes, let's assume if endX is within a certain range, the shot is made
+        //return endX >= 200 && endX <= 300;
+
+        // Hypothetical game logic: Consider the shot made if it falls within a certain distance from the center of the hoop
+
+        // Coordinates of the center of the hoop (hypothetical values for demonstration)
+        //BRO IDK HOW TO FIND THE CENTER OF THE HOOP AHSHIAHISHIAHIHSIJAINBIAHISHI
+        //double hoopCenterX = 50;
+        //double hoopCenterY = 200;
+        //double hoopCenterX = 250;
+        //double hoopCenterY = 150;
+        double hoopCenterX = 168;
+        double hoopCenterY = 318;
+
+        // Calculate the distance between the end coordinates of the shot and the center of the hoop
+        double distance = Math.sqrt(Math.pow(endX - hoopCenterX, 2) + Math.pow(endY - hoopCenterY, 2));
+
+        // Define a threshold distance within which the shot is considered made (hypothetical value for demonstration)
+        double thresholdDistance = 60; // Adjust this value based on your game's requirements
+
+        // If the calculated distance is less than or equal to the threshold distance, consider the shot made
+        return distance <= thresholdDistance;
+    }
+
+    private void updateShotsMadeLabel() {
+        // Update the label to display the number of successful shots
+        shotsMadeLabel.setText("Shots Made: " + successfulShots);
     }
 
 }
+
+//Himani crying over:
+//TRYING TO FIGURE OUT HOW TO ADD THE BBALL IMAGE IDK HOW
+//NEED TO FIGURE OUT HOW TO HAVE PROGRAM IDENTIFY IF SHOT IS MADE OR MISSED AND KEEP COUNT OF IT
+//RN ITS ONLY SHOTS MISSED KMS
+//BRO IDK HOW TO FIND THE CENTER OF THE HOOP AHSHIAHISHIAHIHSIJAINBIAHISHI
+//NEED TO
+//need to still scale this up to fit any screen size but at this point that is a last priority because scene builder
+//will be the end of me
