8c68b2d065b454faa117769b8a61b55ab36fe43b
himanimanju <insaneinkwebdev23@gmail.com>
2024-03-31 20:02:19 -0700

    losing it

--- src/main/java/com/example/javafxprojectcsa/GameView.java
+++ src/main/java/com/example/javafxprojectcsa/GameView.java
@@ -1,5 +1,5 @@
 package com.example.javafxprojectcsa;
-
+import javafx.animation.PathTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -7,23 +7,16 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import java.util.Random;
-
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.QuadCurve;
-
-import javafx.animation.PathTransition;
-import javafx.scene.shape.Line;
 import javafx.util.Duration;
 
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class GameView implements Initializable{
-    private int score;
+public class GameView implements Initializable {
     private double angle;
+    private double[] pathX;
+    private double[] pathY;
     private int successfulShots = 0;
 
     @FXML
@@ -36,194 +29,99 @@ public class GameView implements Initializable{
     ImageView hoopImage;
     @FXML
     ImageView ballImage;
-    @FXML
-    Label shotsMadeLabel;
-    @FXML
-    private BorderPane borderPane;
 
-    ///trying
-    private double[] pathX;
-    private double[] pathY;
-    //private int pathIndex = 0;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Load hoop image
+        Image hoopImg = new Image(getClass().getResourceAsStream("/Image/hoopImage.png"));
+        hoopImage.setImage(hoopImg);
 
-    @FXML
-    public void initialize(URL location, ResourceBundle resources)
-    {
-        File file = new File("src/main/resources/Image/hoopImage.png");
-        Image image = new Image(file.toURI().toString());
-        hoopImage.setImage(image);
-
-        File file1 = new File("src/main/resources/Image/basketball.png");
-        Image image1 = new Image(file1.toURI().toString());
-        ballImage.setImage(image1);
+        // Load basketball image
+        Image ballImg = new Image(getClass().getResourceAsStream("/Image/basketball.png"));
+        ballImage.setImage(ballImg);
     }
 
-    //does the action for the angle button on the screen
     @FXML
     private void onAngleBtnClick() {
-        System.out.println("Enter");
         shootBtn.setVisible(false);
         errorLabel.setText("");
-        //need to hide the shoot button
-        //need to get text/number from textbox
+
         String input = angleText.getText();
-        System.out.println(input);
         int number = 0;
-        //check if it is a valid angle number from 0 to 90
-        //convert text to number
-        //if it is a valid angle, show shoot button and allow the person to shoot
+
         try {
             number = Integer.parseInt(input);
-            System.out.println("Converted integer: " + number);
-            if(number < 0 || number > 90)
-            {
-                System.out.println("Invalid angle input. Please enter a number from 0 to 90");
-                errorLabel.setText("Invalid angle input. Please enter a number from 0 to 90");
-            }
-            else
-            {
+            if (number < 1 || number > 90) {
+                throw new NumberFormatException();
+            } else {
                 errorLabel.setText("Angle entered is: " + number);
-                //want to show button only if angle is correct
+                angle = Math.toRadians(number); // Convert angle to radians
                 shootBtn.setVisible(true);
             }
         } catch (NumberFormatException e) {
-            System.out.println("Invalid integer input. Please enter a number");
-            errorLabel.setText("Invalid integer input. Please enter a number");
+            errorLabel.setText("Invalid angle input. Please enter a number between 1 and 90.");
         }
 
-        angle = number*Math.PI/180;
         angleText.clear();
     }
 
     @FXML
-    private void onShootBtnClick(){
-        System.out.println("shoot");
-        //need to calculate shot using angle and then move the image
-        double endY;
-        double endX;
-        double midX;
-        double midY;
+    private void onShootBtnClick() {
         double startX = ballImage.getX();
-        double startY = ballImage.getY();
-        int v1 = 20;
-        endX = 100*(2*Math.pow(v1, 2)*Math.cos(angle)*Math.sin(angle))/9.8;
-        endY = 0;
-        System.out.println(endX + ", " + endY);
-        //double y = startX/(v1*Math.cos(angle));
-        //midX = (startX + endX)/2;
-        //midY = startY - (startX*Math.tan(angle) + 0.5*9.8*Math.pow(y, 2));
+        double startY = 100;
+        //double startY = ballImage.getY();
+        double endX = hoopImage.getX() + hoopImage.getImage().getWidth() / 2;
+        double endY = hoopImage.getY() + hoopImage.getImage().getHeight() / 2;
 
-        //trying
-        // Calculate shot path
         calculateShotPath(startX, startY, endX, endY);
-        traceShotPath(ballImage);
-
-        /////////////////////himani added to see if we could identify if shot is made and record it
-        if (isShotMade(endX, endY)) {
-            System.out.println("Shot made!");
-            successfulShots++;
-            // updateShotsMadeLabel();
-        } else {
-            System.out.println("Shot missed!");
-        }
-        //note only for testing if the label works
-        successfulShots++;
-        shotsMadeLabel.setText("Shots Made: " + successfulShots);
-        shootBtn.setVisible(false);
+        animateShot(ballImage);
     }
 
-    //trying
     private void calculateShotPath(double startX, double startY, double endX, double endY) {
-        // Calculate the number of points along the shot path
-        //should actually base it on the time it takes the ball to fall
-        //we can calculate that using the formula to find how much time it would actually take for the
-        //ball to fall
-        //then use maybe 100 points divided from that time in order to calculate for x and y
-        //cuz then it will be more accurate
-        //not sure if we wnat to go through the trouble or not though
         int numPoints = 100;
         pathX = new double[numPoints];
         pathY = new double[numPoints];
 
-        // Calculate coordinates of each point along the shot path
+        double a = -(endY - startY) / Math.pow((endX - startX) / 2, 2);
+
         for (int i = 0; i < numPoints; i++) {
             double t = (double) i / (numPoints - 1);
-            //x should increase a constant amount every time
-            double x = (1 - t) * startX + t * endX;
-            //y should increase, then decrease because it is a parabola
-            //so maybe do an if statement--if i <= numpoints/2, then y increase
-            //but if i > numpoints/2, then y decrease
-            //idk how the pathX and pathY functions work so im not gonna do anything about that
-            double y = (1 - t) * startY + t * endY;
+            double x = startX + t * (endX - startX);
+            double y = a * Math.pow(x - startX, 2) + startY;
             pathX[i] = x;
             pathY[i] = y;
         }
     }
 
-    private void traceShotPath(ImageView ballImageView) {
-        // Create a PathTransition to animate the basketball along the shot path
-        PathTransition pathTransition = new PathTransition();
-        pathTransition.setNode(ballImageView);
-
-        // Create a Line object representing the shot path
-        Line path = new Line();
+    private void animateShot(ImageView ballImageView) {
+        QuadCurve path = new QuadCurve();
         path.setStartX(pathX[0]);
         path.setStartY(pathY[0]);
-        path.setEndX(pathX[1]);
-        path.setEndY(pathY[1]);
+        path.setControlX(pathX[pathX.length / 2]);
+        path.setControlY(pathY[pathY.length / 2] + 50);
+        path.setEndX(pathX[pathX.length - 1]);
+        path.setEndY(pathY[pathY.length - 1]);
 
-        // Set the duration of the path transition
-        pathTransition.setDuration(Duration.seconds(2)); // Adjust duration as needed
-
-        // Set the path of the path transition
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setNode(ballImageView);
         pathTransition.setPath(path);
+        pathTransition.setDuration(Duration.seconds(2));
 
-        // Play the path transition animation
+        pathTransition.setOnFinished(event -> {
+            if (isShotMade()) {
+                successfulShots++;
+                errorLabel.setText("Shot made! Total shots made: " + successfulShots);
+            } else {
+                errorLabel.setText("Shot missed!");
+            }
+            shootBtn.setVisible(false);
+        });
+
         pathTransition.play();
     }
 
-    /////////////////used chatgpt for this to get started - one way to identify if shot is made or not
-    private boolean isShotMade(double endX, double endY) {
-        // Logic to determine if the shot is made based on end coordinates
-        // Example: Check if the end coordinates are within the hoop area
-        // You need to implement this method according to your game's logic
-        // For demonstration purposes, let's assume if endX is within a certain range, the shot is made
-        //return endX >= 200 && endX <= 300;
-
-        // Hypothetical game logic: Consider the shot made if it falls within a certain distance from the center of the hoop
-
-        // Coordinates of the center of the hoop (hypothetical values for demonstration)
-        //BRO IDK HOW TO FIND THE CENTER OF THE HOOP AHSHIAHISHIAHIHSIJAINBIAHISHI
-        //double hoopCenterX = 50;
-        //double hoopCenterY = 200;
-        //double hoopCenterX = 250;
-        //double hoopCenterY = 150;
-        double hoopCenterX = 168;
-        double hoopCenterY = 318;
-
-        // Calculate the distance between the end coordinates of the shot and the center of the hoop
-        double distance = Math.sqrt(Math.pow(endX - hoopCenterX, 2) + Math.pow(endY - hoopCenterY, 2));
-
-        // Define a threshold distance within which the shot is considered made (hypothetical value for demonstration)
-        double thresholdDistance = 60; // Adjust this value based on your game's requirements
-
-        // If the calculated distance is less than or equal to the threshold distance, consider the shot made
-        return distance <= thresholdDistance;
+    private boolean isShotMade() {
+        // Determine if the ball's final position intersects with the hoop's position
+        return ballImage.getBoundsInParent().intersects(hoopImage.getBoundsInParent());
     }
-    //don't really need this
-    /*private void updateShotsMadeLabel() {
-        // Update the label to display the number of successful shots
-        shotsMadeLabel.setText("Shots Made: " + successfulShots);
-    }
-*/
 }
-
-//Himani crying over:
-//TRYING TO FIGURE OUT HOW TO ADD THE BBALL IMAGE IDK HOW
-//NEED TO FIGURE OUT HOW TO HAVE PROGRAM IDENTIFY IF SHOT IS MADE OR MISSED AND KEEP COUNT OF IT
-//RN ITS ONLY SHOTS MISSED KMS
-//BRO IDK HOW TO FIND THE CENTER OF THE HOOP AHSHIAHISHIAHIHSIJAINBIAHISHI
-//NEED TO
-//need to still scale this up to fit any screen size but at this point that is a last priority because scene builder
-//will be the end of me
-
