88f31030fda27c4275c965628afa06e1736bdbbd
platterss <78766794+platterss@users.noreply.github.com>
2024-04-07 17:07:25 -0700

    init

--- src/main/java/com/example/javafxprojectcsa/Application.java
+++ src/main/java/com/example/javafxprojectcsa/Application.java
@@ -1,19 +1,14 @@
 package com.example.javafxprojectcsa;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
         Scene sceneHome = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Start Screen");
         stage.setScene(sceneHome);
