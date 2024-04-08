a20e7edfd9f997087e0cc5f87aedf58f6b642837
platterss <78766794+platterss@users.noreply.github.com>
2024-04-07 22:14:21 -0700

    Finished?

--- src/main/java/com/example/javafxprojectcsa/GameView.java
+++ src/main/java/com/example/javafxprojectcsa/GameView.java
@@ -10,44 +10,36 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.*;
 
 import javafx.util.Duration;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class GameView implements Initializable {
-    public Button angleBtn;
     private double angle;
     private int successfulShots = 0;
 
     @FXML
-    TextField angleText;
+    private TextField angleText;
     @FXML
-    Label errorLabel;
+    private Label errorLabel;
     @FXML
-    Button shootBtn;
+    private Button shootBtn, continueBtn, angleBtn;
     @FXML
-    ImageView hoopImage;
+    private ImageView hoopImage, ballImage, personImage;
     @FXML
-    ImageView ballImage;
+    private Label shotsMadeLabel, shotResultLabel;
     @FXML
-    Label shotsMadeLabel;
+    public QuadCurve quadCurve;
     @FXML
-    ImageView personImage;
-    @FXML
-    private BorderPane borderPane;
-    @FXML
-    private Label shotResultLabel;
-    @FXML
-    private Circle ballCenterDot;
-    @FXML
-    private Circle hoopCenterDot;
+    public Pane borderPane;
 
+    private double initialX, initialY;
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
         File file = new File("src/main/resources/Image/hoopImage.png");
@@ -62,71 +54,66 @@ public class GameView implements Initializable {
         Image image2 = new Image(file2.toURI().toString());
         personImage.setImage(image2);
 
-        // FOR DEBUGGING PURPOSES, REMOVE ONCE DONE
-        ballCenterDot = new Circle(0, 0, 5, Color.RED); // A small red dot
-        hoopCenterDot = new Circle(0, 0, 5, Color.BLUE); // A small blue dot
-
-        borderPane.getChildren().add(ballCenterDot);
-        borderPane.getChildren().add(hoopCenterDot);
-
-        positionBallCenterDot();
-        positionHoopCenterDot();
+        initialX = ballImage.getLayoutX();
+        initialY = ballImage.getLayoutY();
     }
 
     @FXML
     private void onAngleBtnClick() {
-        shotResultLabel.setVisible(false);
-        errorLabel.setText("");
-        String input = angleText.getText();
+        if (!continueBtn.isVisible()) {
+            shotResultLabel.setVisible(false);
+            errorLabel.setVisible(false);
+            String input = angleText.getText();
 
-        try {
-            double number = Double.parseDouble(input);
+            try {
+                double number = Double.parseDouble(input);
 
-            if (number < 0 || number > 90) {
-                errorLabel.setText("Invalid input. Enter a number from 0 to 90");
-            } else {
-                errorLabel.setText("Angle entered is: " + number);
-                shootBtn.setVisible(true);
+                if (number < 0 || number > 90) {
+                    errorLabel.setText("Invalid input.");
+                    errorLabel.setVisible(true);
+                } else {
+                    errorLabel.setText("Angle entered is: " + number);
+                    shootBtn.setVisible(true);
 
-                angle = number * Math.PI / 180;
-                angleText.clear();
+                    angle = number * Math.PI / 180;
+                    angleText.clear();
+                }
+            } catch (NumberFormatException e) {
+                errorLabel.setText("Invalid input.");
+                errorLabel.setVisible(true);
             }
-        } catch (NumberFormatException e) {
-            errorLabel.setText("Invalid input. Please enter a number.");
         }
-        errorLabel.setVisible(true);
     }
 
     @FXML
     private void onShootBtnClick(){
-        double endY;
-        double endX;
-        double startX = 50;
-        double startY = 150;
-        int v1 = 20;
-        endX = 10 * (2 * Math.pow(v1, 2) * Math.cos(angle) * Math.sin(angle)) / 9.8;
-        endY = 150;
-        System.out.println(endX + ", " + endY);
+        if (!continueBtn.isVisible()) {
+            double endX;
+            double startX = initialX;
+            double startY = initialY;
+            int v1 = 20;
 
-        calculateShotPath(startX, startY, endX, endY, v1);
+            endX = 10 * (2 * Math.pow(v1, 2) * Math.cos(angle) * Math.sin(angle)) / 9.8;
 
-        shotsMadeLabel.setText("Shots Made: " + successfulShots);
-        shotResultLabel.setVisible(true);
-        shootBtn.setVisible(false);
-        errorLabel.setVisible(false);
+            calculateShotPath(startX, startY, endX, v1);
+
+            shotsMadeLabel.setText("Shots Made: " + successfulShots);
+            shootBtn.setVisible(false);
+            errorLabel.setVisible(false);
+        }
     }
 
-    private void calculateShotPath(double startX, double startY, double endX, double endY, int v1) {
+    private void calculateShotPath(double startX, double startY, double endX, int v1) {
         int numPoints = 100;
         double[] pathX = new double[numPoints];
         double[] pathY = new double[numPoints];
+
         double y = startY;
 
-        // Calculate coordinates of each point along the shot path
-        for (int i = 0; i < numPoints; i++) {
-            double x = i*(startX + endX)/numPoints;
+        for (int i = 1; i < numPoints; i++) {
+            double x = i * (startX + endX) / numPoints;
 
-            if(i <= numPoints/2)
+            if (i <= numPoints / 2)
             {
                 y = y - 2;
             } else {
@@ -136,48 +123,46 @@ public class GameView implements Initializable {
             pathY[i] = y;
         }
 
-        double height= startX/(v1*Math.cos(angle));
+        double height = startX/(v1*Math.cos(angle));
         double midX = (startX + endX)/2;
         double midY = startY - 2*(startX*Math.tan(angle) + 0.5*9.8*Math.pow(height, 2));
-        var cpX = 2*midX - pathX[0]/2 - pathX[99]/2;
-        var cpY = 2*midY - pathY[0]/2 - pathY[99]/2;
+        var cpX = 2 * midX - pathX[0]/2 - pathX[99]/2;
+        var cpY = 2 * midY - pathY[0]/2 - pathY[99]/2;
 
-        QuadCurve test = new QuadCurve();
-        test.setStartX(pathX[0]);
-        test.setStartY(pathY[0]);
-        test.setControlX(cpX);
-        test.setControlY(cpY);
-        test.setEndY(pathY[99]);
-        test.setEndX(pathX[99]);
+        quadCurve.setStartX(initialX);
+        quadCurve.setStartY(initialY);
+        quadCurve.setControlX(cpX);
+        quadCurve.setControlY(cpY);
+        quadCurve.setEndY(pathY[99]);
+        quadCurve.setEndX(pathX[99]);
 
         PathTransition pathTransitionTest = new PathTransition();
         pathTransitionTest.setNode(ballImage);
-        pathTransitionTest.setDuration(Duration.seconds(5));
-        pathTransitionTest.setPath(test);
+        pathTransitionTest.setDuration(Duration.seconds(3));
+        pathTransitionTest.setPath(quadCurve);
 
         AnimationTimer collisionChecker = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                Bounds ballBounds = ballImage.getBoundsInParent();
-                double ballCenterX = ballBounds.getMinX() + ballBounds.getWidth() / 2;
-                double ballCenterY = ballBounds.getMinY() + ballBounds.getHeight() / 2 + 40;
-                positionBallCenterDot();
-                positionHoopCenterDot();
+            Bounds ballBounds = ballImage.getBoundsInParent();
+            double ballCenterX = ballBounds.getMinX() + ballBounds.getWidth() / 2;
+            double ballCenterY = ballBounds.getMinY() + ballBounds.getHeight() / 2;
 
-                Bounds hoopBounds = hoopImage.getBoundsInParent();
-                double hoopCenterX = hoopBounds.getMinX() + hoopBounds.getWidth() / 2;
-                double hoopCenterY = hoopBounds.getMinY() + hoopBounds.getHeight() / 2;
+            Bounds hoopBounds = hoopImage.getBoundsInParent();
+            double hoopCenterX = hoopBounds.getMinX() + hoopBounds.getWidth() / 2;
+            double hoopCenterY = hoopBounds.getMinY() + hoopBounds.getHeight() / 2;
 
-                double distance = Math.sqrt(Math.pow(ballCenterX - hoopCenterX, 2) + Math.pow(ballCenterY - hoopCenterY, 2));
+            double distance = Math.sqrt(Math.pow(ballCenterX - hoopCenterX, 2) + Math.pow(ballCenterY - hoopCenterY, 2));
 
-                if (distance < 15) {
-                    successfulShots++;
-                    shotsMadeLabel.setText("Shots Made: " + successfulShots);
+            if (distance < 15) {
+                successfulShots++;
+                shotsMadeLabel.setText("Shots Made: " + successfulShots);
 
-                    this.stop();
-                    shotResultLabel.setText("Nice Shot!");
-                    shotResultLabel.setVisible(true);
-                }
+                this.stop();
+                shotResultLabel.setText("Nice Shot!");
+                shotResultLabel.setVisible(true);
+                continueBtn.setVisible(true);
+            }
             }
         };
 
@@ -187,15 +172,20 @@ public class GameView implements Initializable {
         collisionChecker.start();
     }
 
-    private void positionHoopCenterDot() {
-        Bounds hoopBounds = hoopImage.getBoundsInParent();
-        hoopCenterDot.setCenterX(hoopBounds.getMinX() + hoopBounds.getWidth() / 2);
-        hoopCenterDot.setCenterY(hoopBounds.getMinY() + hoopBounds.getHeight() / 2);
-    }
+    public void onContinueBtnPress() {
+        double minY = hoopImage.getFitHeight();
+        double maxY = 30;
 
-    private void positionBallCenterDot() {
-        Bounds ballBounds = ballImage.getBoundsInParent();
-        ballCenterDot.setCenterX(ballBounds.getMinX() + ballBounds.getWidth() / 2);
-        ballCenterDot.setCenterY(ballBounds.getMinY() + ballBounds.getHeight() / 2 + 40);
+        Random rand = new Random();
+        double newY = minY + (maxY - minY) * rand.nextDouble();
+
+        hoopImage.setLayoutX(rand.nextDouble(150, 432));
+        hoopImage.setLayoutY(newY);
+
+        ballImage.setLayoutX(initialX);
+        ballImage.setLayoutY(initialY);
+
+        continueBtn.setVisible(false);
+        shotResultLabel.setVisible(false);
     }
 }
